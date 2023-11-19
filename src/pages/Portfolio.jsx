eea6aeaa1beb77f8b5a0122b9c7ce20df69e7db4
Zach Hamilton <zachmhamilton@outlook.com>
2023-11-19 17:00:26 -0500

    added project images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,6 @@
 import Nav from '../components/nav';
 import { useTheme } from '@mui/material/styles';
+import EastIcon from '@mui/icons-material/East';
 
 const Portfolio = () => {
   const theme = useTheme();
@@ -15,15 +16,63 @@ const Portfolio = () => {
         >
           Portfolio
         </h1>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <span
+              style={{
+                color: theme.palette.accent.main,
+                fontSize: '1.5em',
+              }}
+            >
+              Languages&nbsp;
+            </span>
+            <p
+              style={{
+                color: 'white',
+              }}
+            >
+              &nbsp; JavaScript | TypeScript | SQL | Java
+            </p>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <span
+              style={{
+                color: theme.palette.accent.main,
+                fontSize: '1.5em',
+              }}
+            >
+              Technologies&nbsp;
+            </span>
+            <p
+              style={{
+                color: 'white',
+              }}
+            >
+              &nbsp; React | Node | Express | AWS SDK | PostgreSQL | MongoDB |
+              Redux | Git | Jest | HTML | CSS | JWT | Heroku
+            </p>
+          </div>
+        </div>
         <div id="features">
-          <h1>Features</h1>
           <div className="feature">
             <div className="featureDiscription">
-              <h2 style={{ color: theme.palette.accent.main }}>
+              <h2
+                style={{ color: theme.palette.accent.main, fontSize: '2.5em' }}
+              >
                 Lambda Peeler
               </h2>
               <p>
-                Lambda Peeler is a web-based dashboard tailored for AWS Lambda
+                Lambda Peeler is an open-source, web-based dashboard tailored for AWS Lambda
                 developers. It is meticulously designed to bridge the gap
                 between managing Lambda functions and layers, simplifying AWS
                 cloud operations. It enhances developing efficiency by allowing
@@ -32,12 +81,28 @@ const Portfolio = () => {
                 very robust CI/CD pipeline through AWS. Our product aims to
                 bridge the gap and give engineers an easy-to-use alternative.
               </p>
+              <a
+                href="https://lambda-peeler.onrender.com/"
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '.1em',
+                  color: theme.palette.accent.main,
+                }}
+              >
+                Website <EastIcon color={theme.palette.accent.main} />
+              </a>
             </div>
-            {/* <img id="gif" src="/assets/connecting.gif"></img> */}
+            <img id="gif" src="../src/assets/connecting.gif"></img>
           </div>
           <div className="feature">
+            <img id="gif" src="../src/assets/library.png"></img>
             <div className="featureDiscription">
-              <h2 style={{ color: theme.palette.accent.main }}>Shelved</h2>
+              <h2
+                style={{ color: theme.palette.accent.main, fontSize: '2.5em' }}
+              >
+                Shelved
+              </h2>
               <p>
                 Shevled is a digital library app for avid readers, audiobook
                 listeners, and e-book enjoyers. It can be hard to keep track of
@@ -47,12 +112,26 @@ const Portfolio = () => {
                 GoogleBooks API that allows users to view more information about
                 each book, such as author, genre, and number of pages.
               </p>
+              <a
+                href="https://github.com/ZachMHamilton/Library-Project"
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '.1em',
+                  color: theme.palette.accent.main,
+                }}
+              >
+                Docs <EastIcon color={theme.palette.accent.main} />
+              </a>
             </div>
-            {/* <img id="gif" src="/assets/failing.gif"></img> */}
           </div>
           <div className="feature">
             <div className="featureDiscription">
-              <h2 style={{ color: theme.palette.accent.main }}>Algo Archive</h2>
+              <h2
+                style={{ color: theme.palette.accent.main, fontSize: '2.5em' }}
+              >
+                Algo Archive
+              </h2>
               <p>
                 AlgoArchive is a web-based dashboard that allows users to track
                 their algorithm practice in preparation for technical
@@ -60,10 +139,32 @@ const Portfolio = () => {
                 and their solution, as well as any notes they would like to
                 save, such as difficulty, different strategies used, and
               </p>
+              <a
+                href="https://github.com/danger-noodle-42/algo-archive"
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '.1em',
+                  color: theme.palette.accent.main,
+                }}
+              >
+                Docs <EastIcon color={theme.palette.accent.main} />
+              </a>
             </div>
-            {/* <img id="gif" src="/assets/removing.gif"></img> */}
+            <img id="gif" src="../src/assets/algo.png"></img>
           </div>
         </div>
+        <p style={{ textAlign: 'center' }}>
+          View my full resume{' '}
+          <a
+            href="https://drive.google.com/file/d/19ZmLDX3Qo5mCAFUohqiXF2_jg_FuQNiv/view?usp=sharing"
+            style={{
+              color: theme.palette.accent.main,
+            }}
+          >
+            here
+          </a>
+        </p>
       </div>
     </div>
   );
